1ddf2d87ee681bc504f7d7ecff49d7908cd4dfa1
Jacob Simpson <simpson.jacob.barry@gmail.com>
2021-04-20 17:29:15 -0400

    Added jobs model

---
+++ models/jobs.py
@@ -0,0 +1,36 @@
+from db import db
+
+
+class JobModel(db.Model):
+  """
+  ============
+  Job Postings
+  ============
+  Job postings scraped from various job boards using scrapy
+
+  Job table contains:
+  * id: primary key
+  * title
+  * description
+  * company_name
+  * salary
+  * rating
+  * number_of_ratings
+  * location
+  * source
+  * url: unique value
+
+  Public methods:
+  """
+  __tablename__ = 'jobs'
+
+  id = db.Column(db.Integer, primary_key = True)
+  title = db.Column(db.String())
+  description = db.Column(db.String())
+  company_name = db.Column(db.String())
+  salary = db.Column(db.Float(precision = 2))
+  rating = db.Column(db.Float(precision = 1))
+  number = db.Column(db.Integer())
+  location = db.Column(db.String())
+  source = db.Column(db.String())
+  url = db.Column(db.String(), unique = True)
--- models/user.py
+++ models/user.py
@@ -1,6 +1,21 @@
 from db import db
 
 class UserModel(db.Model):
+  """
+  ============
+  User's Table
+  ============
+
+  User table contains:
+  * id: primary key
+  * email: unique value
+  * password: hashed value
+
+  Public methods:
+  1. save_to_db: saves the user to the database
+  2. find_by_username: finds the user based on email(username)
+  3. find_by_id: finds the user based on user id
+  """
   __tablename__ = 'users'
 
   id = db.Column(db.Integer, primary_key = True)
@@ -20,8 +35,18 @@ class UserModel(db.Model):
 
   @classmethod
   def find_by_username(cls, email): # username mapping
+    """
+    find_by_username: finds the user by their email\n
+    :param email: unique email for user to be found\n
+    `:returns: user`
+    """
     return cls.query.filter_by(email = email).first()
 
   @classmethod
   def find_by_id(cls, _id): # userid mapping
+    """
+    find_by_id: finds the user by their id\n
+    :param _id: unique email for user to be found\n
+    `:returns: user`
+    """
     return cls.query.filter_by(id = _id).first()
